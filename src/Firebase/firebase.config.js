bd6ef7362366fe0d9bfe0ed3af23ad74d8952fbf
Rezwan <t.rewan17@gmail.com>
2023-04-17 02:00:58 +0600

    Firebase shared context api project created

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5ZKDpv585FzJyV-eJ1eREoM_pBHLALyQ",
+  authDomain: "fir-auth-context-fa45d.firebaseapp.com",
+  projectId: "fir-auth-context-fa45d",
+  storageBucket: "fir-auth-context-fa45d.appspot.com",
+  messagingSenderId: "232075614204",
+  appId: "1:232075614204:web:fa86686d7296fe65c78479"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
